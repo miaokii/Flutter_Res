beb045b6f2b1199bc04d5b4ee84eda0e7ba25cf2
kangkai <kangkai1108@sina.cn>
2021-04-02 16:22:54 +0800

    login vaild

--- Flutter_demo/github_client/lib/routes/login_page.dart
+++ Flutter_demo/github_client/lib/routes/login_page.dart
@@ -1,7 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:github_client/common/api_type.dart';
 import 'package:github_client/index.dart';
+import 'package:github_client/main.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -13,12 +13,32 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
+  // 登录人员角色
   int _role = 1;
-  void _login() {
-    Navigator.of(context).pushNamed('main');
+  // 登录方式
+  bool _pwdLogin = true;
+  // 输入组件
+  LoginInputWidget _input;
+
+  @override
+  void initState() {
+    super.initState();
+    _input = LoginInputWidget((isPwd) {
+      _pwdLogin = isPwd;
+    });
   }
 
+  // 登录
+  void _login() {
+    final vaild = _input.vaildInput;
+    if (vaild.isNotEmpty) {
+      HUD.flash(vaild);
+    } else {
+      Navigator.of(context).pushNamed('main');
+    }
+  }
+
+  // 登录角色更改
   void _setRole(int role) {
     setState(() {
       _role = role;
@@ -27,59 +47,63 @@ class _LoginPageState extends State<LoginPage> {
 
   List<Widget> _bodyWidgets() {
     return [
-      Padding(padding: EdgeInsets.only(top: 77.r),
-        child: Image(image: AssetImage('assets/images/login_logo.png'),
-            width: 90.w,
-            height: 90.h
-        ),
+      Padding(
+        padding: EdgeInsets.only(top: 77.r),
+        child: Image(
+            image: AssetImage('assets/images/login_logo.png'),
+            width: 90.r,
+            height: 90.r),
       ),
-      Padding(padding: EdgeInsets.only(top: 10.r),
-          child: LoginRoleWidget((role) => _setRole(role))
+      Padding(
+          padding: EdgeInsets.only(top: 10.r),
+          child: LoginRoleWidget((role) => _setRole(role))),
+      Padding(
+        padding: EdgeInsets.only(top: 10.r),
+        child: _input,
       ),
-      Padding(padding: EdgeInsets.only(top: 10.r),
-        child: LoginInputWidget(),),
-      Padding(padding: EdgeInsets.only(top: 45.r, left: 30, right: 30),
+      Padding(
+          padding: EdgeInsets.only(top: 50.r, left: 30, right: 30),
           child: Row(
             children: [
               Expanded(
-                child: MaterialButton(child: Text('登 录', style: TextStyle(fontSize: 18),),
+                child: MaterialButton(
+                  child: Text(
+                    '登 录',
+                    style: TextStyle(fontSize: 18),
+                  ),
                   textColor: Colors.white,
                   height: 40,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
                   color: mainColor,
                   onPressed: _login,
                 ),
               )
             ],
-          )
-      )
+          ))
     ];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         body: Container(
             decoration: BoxDecoration(
               image: DecorationImage(
                   image: AssetImage('assets/images/login_bg.png'),
-                  fit: BoxFit.cover
-              ),
+                  fit: BoxFit.cover),
             ),
             // 在安全区域内布局
             child: SafeArea(
                 child: Column(
                     mainAxisSize: MainAxisSize.max,
-                    children: _bodyWidgets()
-                )
-            )
-        )
-    );
+                    children: _bodyWidgets()))));
   }
 }
 
+// ignore: must_be_immutable
 class LoginRoleWidget extends StatefulWidget {
-
   LoginRoleWidget(this._onSelectedRole);
   Function(int) _onSelectedRole;
 
@@ -90,8 +114,8 @@ class LoginRoleWidget extends StatefulWidget {
 }
 
 class _LoginRoleState extends State<LoginRoleWidget> {
-
   Function get _onSelectedRole => widget._onSelectedRole;
+
   /// 1是云厨房工作人员、2是门店人员
   int _role = 1;
 
@@ -123,7 +147,8 @@ class _LoginRoleState extends State<LoginRoleWidget> {
               children: [
                 Column(
                   children: [
-                    CusTextButton('云厨房工作人员',
+                    CusTextButton(
+                      '云厨房工作人员',
                       fontSize: _role == 1 ? 18 : 15,
                       onPress: _roleStaffSwitch,
                     ),
@@ -136,7 +161,8 @@ class _LoginRoleState extends State<LoginRoleWidget> {
                 ),
                 Column(
                   children: [
-                    CusTextButton('门店人员',
+                    CusTextButton(
+                      '门店人员',
                       fontSize: _role == 2 ? 18 : 15,
                       onPress: _roleStoresSwitch,
                     ),
@@ -150,76 +176,111 @@ class _LoginRoleState extends State<LoginRoleWidget> {
               ],
             ),
           ],
-        )
-    );
+        ));
   }
 }
 
+// ignore: must_be_immutable
 class LoginInputWidget extends StatefulWidget {
+  LoginInputWidget(this.loginTypeSwitch);
+  Function(bool) loginTypeSwitch;
 
-  Function(bool) _loginTypeSwitch;
+  String get tel => _state.tel;
+  String get pwdOrCode => _state.pwdOrCode;
+  String get vaildInput => _state.vaildInput();
+
+  final _state = _LoginInputState();
 
   @override
   State<StatefulWidget> createState() {
-    return _LoginInputState();
+    return _state;
   }
 }
 
 class _LoginInputState extends State<LoginInputWidget> {
-
   /// 是否是验证码登录
   bool _pwdLogin = true;
-  /// 登录类型切换回调
-  Function get _loginTypeSwitch => widget._loginTypeSwitch;
 
-  void _pwdSwitch(){
+  TextEditingController _telController = TextEditingController();
+  TextEditingController _pwdCodeController = TextEditingController();
+
+  String get tel {
+    return _telController.text;
+  }
+
+  String get pwdOrCode {
+    return _pwdCodeController.text;
+  }
+
+  String vaildInput() {
+    if (tel.length != 11) {
+      return '请输入正确的电话号码!';
+    } else if (pwdOrCode.isEmpty) {
+      return '请输入${_pwdLogin ? '密码' : '验证码'}!';
+    }
+    return '';
+  }
+
+  void _pwdSwitch() {
     setState(() {
       _pwdLogin = !_pwdLogin;
-      // _loginTypeSwitch(_vCodeLogin);
+      widget.loginTypeSwitch(_pwdLogin);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.only(left: 30, right: 30, top: 10),
-        // color: Colors.red,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            TextFormField(
-                decoration: InputDecoration(
-                    hintText: _pwdLogin ? '请输入用户名' : '请输入手机号',
-                    icon: Icon(_pwdLogin ? Icons.person : Icons.phone_android_outlined, color: mainColor),
-                    border: InputBorder.none
-                )
-            ),
-            Divider(height: 1,),
-            Padding(padding: EdgeInsets.only(top: 5),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    hintText: _pwdLogin ? '请输入密码': '请输入验证码',
-                    icon: Icon(_pwdLogin ? Icons.lock : Icons.verified_user_sharp, color: mainColor,),
-                    border: InputBorder.none,
-                    suffix: !_pwdLogin ? CusTextButton('发送验证码',
-                      fontSize: 12,
-                      textColor: mainColor,
-                      onPress: (){},) : null,
-                  ),
-                  obscureText: _pwdLogin,
-                )
-            ),
-            Divider(height: 1,),
-            CusTextButton(_pwdLogin ? '验证码登录' : '密码登录',
-                textColor: Colors.black54,
-                fontSize: 13,
-                onPress: _pwdSwitch),
-          ],
-        )
+      padding: EdgeInsets.only(left: 30, right: 30, top: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          TextField(
+            keyboardType: TextInputType.number,
+            controller: _telController,
+            decoration: InputDecoration(
+                hintText: _pwdLogin ? '请输入用户名' : '请输入手机号',
+                icon: Icon(
+                    _pwdLogin ? Icons.person : Icons.phone_android_outlined,
+                    color: mainColor),
+                border: InputBorder.none,
+                errorStyle: TextStyle(color: Colors.black)),
+          ),
+          Divider(
+            height: 1,
+          ),
+          SizedBox(height: 10),
+          TextField(
+              controller: _pwdCodeController,
+              decoration: InputDecoration(
+                hintText: _pwdLogin ? '请输入密码' : '请输入验证码',
+                icon: Icon(
+                  _pwdLogin ? Icons.lock : Icons.verified_user_sharp,
+                  color: mainColor,
+                ),
+                border: InputBorder.none,
+                suffix: !_pwdLogin
+                    ? CusTextButton(
+                        '发送验证码',
+                        fontSize: 12,
+                        textColor: mainColor,
+                        onPress: () {},
+                      )
+                    : null,
+              ),
+              obscureText: _pwdLogin),
+          Divider(
+            height: 1,
+          ),
+          CusTextButton(_pwdLogin ? '验证码登录' : '密码登录',
+              textColor: Colors.black54, fontSize: 13, onPress: _pwdSwitch),
+        ],
+      ),
     );
   }
 }
 
+/*
 class LoginRoute extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -228,13 +289,13 @@ class LoginRoute extends StatefulWidget {
 }
 
 class _LoginRouteState extends State<LoginRoute> {
-
   TextEditingController _nameField = TextEditingController();
   TextEditingController _pwdField = TextEditingController();
 
   /// 是否显示密码
   bool pwdShow = false;
   GlobalKey _formKey = new GlobalKey<FormState>();
+
   /// 用户框第一响应
   bool _nameAutoFocus = true;
 
@@ -254,10 +315,11 @@ class _LoginRouteState extends State<LoginRoute> {
       HUD.loading(context);
       Map param = {
         "password": _pwdField.text,
-        "type":1,
+        "type": 1,
         "username": _nameField.text,
       };
-      Result res = await NetReq.request(API.pwdLogin, params: param, method: Method.POST);
+      Result res = await NetReq.request(API.pwdLogin,
+          params: param, method: Method.POST);
       HUD.hide(context);
       if (200 == res.code) {
         User user = User.fromJson(res.data);
@@ -287,10 +349,9 @@ class _LoginRouteState extends State<LoginRoute> {
                 autofocus: _nameAutoFocus,
                 controller: _nameField,
                 decoration: InputDecoration(
-                  hintText: '用户名或邮箱',
-                  labelText: '用户名',
-                  icon: Icon(Icons.person)
-                ),
+                    hintText: '用户名或邮箱',
+                    labelText: '用户名',
+                    icon: Icon(Icons.person)),
                 validator: (value) {
                   return value.trim().isNotEmpty ? null : '用户名不能为空';
                 },
@@ -298,33 +359,36 @@ class _LoginRouteState extends State<LoginRoute> {
               TextFormField(
                 controller: _pwdField,
                 decoration: InputDecoration(
-                  labelText: '密码',
-                  hintText: '登录密码',
-                  icon: Icon(Icons.lock),
-                  suffix: IconButton(
-                    icon: Icon(pwdShow ? Icons.remove_red_eye_outlined : Icons.remove_red_eye,
-                    color: Colors.blue,),
-                    onPressed: (){
-                      setState(() {
-                        pwdShow = !pwdShow;
-                      });
-                    },
-                  )
-                ),
+                    labelText: '密码',
+                    hintText: '登录密码',
+                    icon: Icon(Icons.lock),
+                    suffix: IconButton(
+                      icon: Icon(
+                        pwdShow
+                            ? Icons.remove_red_eye_outlined
+                            : Icons.remove_red_eye,
+                        color: Colors.blue,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          pwdShow = !pwdShow;
+                        });
+                      },
+                    )),
                 obscureText: !pwdShow,
                 validator: (pwd) {
                   return pwd.trim().length > 4 ? null : '密码不少于4位';
                 },
               ),
-              Padding(padding: EdgeInsets.only(top: 20),
-                child: RaisedButton(
-                  padding: EdgeInsets.only(left: 20, right: 20),
-                  child: Text('登录'),
-                  color: Theme.of(context).primaryColor,
-                  textColor: Colors.white,
-                  onPressed: _passLogin,
-                )
-              )
+              Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: RaisedButton(
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    child: Text('登录'),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Colors.white,
+                    onPressed: _passLogin,
+                  ))
             ],
           ),
         ),
@@ -332,3 +396,4 @@ class _LoginRouteState extends State<LoginRoute> {
     );
   }
 }
+*/
